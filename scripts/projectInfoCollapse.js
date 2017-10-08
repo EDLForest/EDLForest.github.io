9739c26aeb87ce5836f78c2ed98a0a6c2bf0ef7d
HaoSen Zheng <hzheng20@binghamton.edu>
2017-10-08 13:09:49 -0400

    Update projectInfoCollapse.js
    Trying to fix an error where $().Blah is not recognized as a function.

--- scripts/projectInfoCollapse.js
+++ scripts/projectInfoCollapse.js
@@ -1,16 +1,20 @@
-console.log("projectInfoCollapse started");
-$(".proj-learn-more").click(function(e) {
-    e.preventDefault();
-    var target = $(this).attr("href");
-    console.log(target);
-    $(".proj-info").each(function() {
-//        console.log($(this).attr("id"));
-        if("#" + $(this).attr("id") == target) {
-            $(this).slideToggle(500);
-            console.log("showing" + $(this).attr("id"));
-        }
-        else {
-            $(this).slideUp(500);
-        }
+jQuery(document).ready(function($){
+
+    console.log("projectInfoCollapse started");
+    $(".proj-learn-more").click(function(e) {
+        e.preventDefault();
+        var target = $(this).attr("href");
+        //console.log(target);
+        $(".proj-info").each(function() {
+    //        console.log($(this).attr("id"));
+            if("#" + $(this).attr("id") == target) {
+                $(this).slideToggle(500);
+                //console.log("showing" + $(this).attr("id"));
+            }
+            else {
+                $(this).slideUp(500);
+            }
+        });
     });
+    
 });
