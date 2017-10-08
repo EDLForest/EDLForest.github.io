80cb8e014cd6776b4e78d1c36104c1fe7f3be67f
hzheng20 <haosenzheng@gmail.com>
2017-10-08 12:39:14 -0400

    Updated Webapge with links and contents

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a personal webpage for Haosen Zheng.
+The original code is on Cloud 9 IDE.
+
---
+++ scripts/projectInfoCollapse.js
@@ -0,0 +1,16 @@
+console.log("projectInfoCollapse started");
+$(".proj-learn-more").click(function(e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    console.log(target);
+    $(".proj-info").each(function() {
+//        console.log($(this).attr("id"));
+        if("#" + $(this).attr("id") == target) {
+            $(this).slideToggle(500);
+            console.log("showing" + $(this).attr("id"));
+        }
+        else {
+            $(this).slideUp(500);
+        }
+    });
+});
